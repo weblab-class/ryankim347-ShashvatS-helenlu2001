15e28f47c54a21292c4caf772537f223030577ac
Helen Lu <helenlu2001@gmail.com>
2021-01-21 22:09:26 -0800

    fixed homepage animation + prettified how to

--- client/src/components/pages/Join.js
+++ client/src/components/pages/Join.js
@@ -83,24 +83,62 @@ class Join extends Component {
       <>
         <NavBar/>
         <div className="Join-container">
-          <div className="Join-titleContainer">
-            <input className = "Join-input" placeholder="name" value={this.state.name} onChange={this.onNameChange} />
-            <br></br>
-            <input className = "Join-input" placeholder="enter game code" value={this.state.code} onChange={this.onChange} />
-            <div className="Join-spacer"> </div>
-            <div className="Join-button" onClick={this.onJoin}>
-              {" "}
-              J O I N{" "}
+          <div className='Join-fieldContainer'>
+            <div className="Join-titleContainer">
+              <input className = "Join-input" placeholder="name" value={this.state.name} onChange={this.onNameChange} />
+              <br></br>
+              <input className = "Join-input" placeholder="enter game code" value={this.state.code} onChange={this.onChange} />
+              <div className="Join-spacer"> </div>
+              <div className="Join-button" onClick={this.onJoin}>
+                {" "}
+                J O I N{" "}
+              </div>
+              <div className="Join-spacer"> </div>
+              <div className="Join-button" onClick={this.onCreate}>
+                {" "}
+                C R E A T E{" "}
+              </div>
+
             </div>
-            <div className="Join-spacer"> </div>
-            <div className="Join-button" onClick={this.onCreate}>
-              {" "}
-              C R E A T E{" "}
+          </div>
+          <div className='Join-howTo'>
+            <div className='u-heading u-textCenter' style={{marginBottom: 16}}> — HOW TO PLAY — </div>
+            <div className='Join-row'>
+              <div className='Join-field' style={{flexGrow: 2, borderRight: '1px solid white'}}>
+                <div className='Join-fieldTitle'> Goal </div>
+                <div> win as many points as possible by tagging others with your laser </div>
+              </div>
+              <div className='Join-field' >
+                <div className='Join-fieldTitle'> Players </div>
+                <div className='u-textCenter'> 2 to 12 </div>
+              </div>
             </div>
-            <div className="Join-spacer"> </div>
-            <div className="Join-button" onClick={this.navRules}>
-              {" "}
-              H O W &nbsp;T O &nbsp;P L A Y{" "}
+            <hr style={{margin: 0, marginTop: 4, height: 1, border: 'none'}}/>
+            <div className='Join-field'>
+                <div className='Join-fieldTitle'> Gameplay </div>
+                <ul>
+                  <li>point mouse in the direction to move towards</li>
+                  <li>press <span className='Join-spacebar'> SPACEBAR</span> to shoot your laser</li>
+                </ul>
+            </div>
+            <hr style={{margin: 0, marginTop: 4, height: 0.5, border: 'none'}}/>
+            <div className='Join-row'>
+              <div className='Join-field' style={{flexGrow: 1, borderRight: '1px solid white'}}>
+                <div className='Join-fieldTitle'> Powerups </div>
+                <ul>
+                  <li> invisiblity </li>
+                  <li> speed up   </li>
+                  <li> shrink</li>
+
+                </ul>
+              </div>
+              <div className='Join-field' style={{flexGrow: 1}} >
+                <div className='Join-fieldTitle'> Map Elements </div>
+                <ul>
+                  <li> <span> <div style={{border: '1px solid white', backgroundColor: 'white', width: 8, height: 8, marginBottom: 0, marginRight: 4, display: "inline-block"}}> </div> </span> walls stop lasers </li>
+                  <li> <span> <div style={{border: '1px solid white', width: 8, height: 8, marginBottom: 0, marginRight: 4, display: "inline-block"}}> </div> </span> mirrors reflect lasers </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -110,3 +148,6 @@ class Join extends Component {
 }
 
 export default Join;
+
+// laser will stop at blocks and bounce off of mirrors (the hollow boxes).
+// Also, collect power ups: red makes you smaller and harder to hit, gray makes you slower, and yellow makes you faster.
