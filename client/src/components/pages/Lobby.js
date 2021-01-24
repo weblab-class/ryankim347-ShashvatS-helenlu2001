fd95520cef5dfb347a7ed268b7a1e345cd32638a
Shashvat Srivastava <shashvat.sri@gmail.com>
2021-01-23 21:29:41 -0500

    added mor instructions to the loading screen

--- client/src/components/pages/Lobby.js
+++ client/src/components/pages/Lobby.js
@@ -16,8 +16,21 @@ function myId() {
   return cookie.parse(document.cookie)["client-id"];
 }
 
-const COLORS = ['#FF00D0', '#FFFF00', '#00FF00', '#00D0FF', '#FFAA00', '#BB00FF', '#FED4FF', '#FFFFAB', '#C4FFC4', '#C2F4FF', '#FFE1A6', '#D1D1D1'];
-const SIZE = ['small', 'medium', 'large'];
+const COLORS = [
+  "#FF00D0",
+  "#FFFF00",
+  "#00FF00",
+  "#00D0FF",
+  "#FFAA00",
+  "#BB00FF",
+  "#FED4FF",
+  "#FFFFAB",
+  "#C4FFC4",
+  "#C2F4FF",
+  "#FFE1A6",
+  "#D1D1D1",
+];
+const SIZE = ["small", "medium", "large"];
 const RADIUS = [10, 12, 14];
 
 class Lobby extends Component {
@@ -33,7 +46,7 @@ class Lobby extends Component {
       playerNames: undefined,
       myName: this.props.location.state.name,
       myColor: undefined,
-      display: 'LOBBY',
+      display: "LOBBY",
       browseMaps: false,
 
       standard: true,
@@ -52,7 +65,6 @@ class Lobby extends Component {
       respawn: 3,
       speed: 4,
       size: 1,
-
     };
 
     this.lobbyData = this.lobbyData.bind(this);
@@ -126,14 +138,20 @@ class Lobby extends Component {
       colors: data.colors,
     });
 
-    let playerID = Object.keys(data.playerNames).find(key => data.playerNames[key] === this.state.myName);
-    let colorKey = Object.keys(data.colors).find(key => data.colors[key] === playerID);
-    this.setState({myColor: COLORS[colorKey]});
+    let playerID = Object.keys(data.playerNames).find(
+      (key) => data.playerNames[key] === this.state.myName
+    );
+    let colorKey = Object.keys(data.colors).find((key) => data.colors[key] === playerID);
+    this.setState({ myColor: COLORS[colorKey] });
   }
 
   getColor(value) {
-    let playerID = Object.keys(this.state.playerNames).find(key => this.state.playerNames[key] === value);
-    let colorKey = Object.keys(this.state.colors).find(key => this.state.colors[key] === playerID);
+    let playerID = Object.keys(this.state.playerNames).find(
+      (key) => this.state.playerNames[key] === value
+    );
+    let colorKey = Object.keys(this.state.colors).find(
+      (key) => this.state.colors[key] === playerID
+    );
     return COLORS[colorKey];
   }
 
@@ -152,14 +170,14 @@ class Lobby extends Component {
           mirrors: Array.from(this.state.custMirrors),
 
           // game settings
-          speed: this.state.speed <= 4 ? this.state.speed / 4 : 2*(this.state.speed-4),
+          speed: this.state.speed <= 4 ? this.state.speed / 4 : 2 * (this.state.speed - 4),
           size: RADIUS[this.state.size],
-          respawn: this.state.respawn*5,
-          cooldown: this.state.cooldown <= 4 ? this.state.cooldown / 4 : this.state.cooldown / 2 -1,
+          respawn: this.state.respawn * 5,
+          cooldown:
+            this.state.cooldown <= 4 ? this.state.cooldown / 4 : this.state.cooldown / 2 - 1,
 
           duration: this.state.duration,
-
-        }
+        },
       });
       this.props.setDuration(this.state.duration);
     }
@@ -167,14 +185,14 @@ class Lobby extends Component {
 
   receiveStartGame(startTime, duration) {
     this.props.setDuration(duration);
-    navigate("/game", {state: {startTime: startTime, color: this.state.myColor}});
+    navigate("/game", { state: { startTime: startTime, color: this.state.myColor } });
   }
 
   updateDisplay(display) {
-    this.setState({display: display});
+    this.setState({ display: display });
   }
 
-  selectMap(title, width, height, blocks,mirrors) {
+  selectMap(title, width, height, blocks, mirrors) {
     this.setState({
       browseMaps: title.length === 0,
       custTitle: title,
@@ -182,144 +200,249 @@ class Lobby extends Component {
       custHeight: height,
       custBlocks: blocks,
       custMirrors: mirrors,
-    })
+    });
   }
 
   render() {
     // makes the game code look prettier
     let code = [];
     for (let i = 0; i < this.props.code.length; i++) {
-      code.push(<span style={{margin: 12}}>{this.props.code[i]}</span>);
+      code.push(<span style={{ margin: 12 }}>{this.props.code[i]}</span>);
     }
 
     let grid = [];
-    for(let i = 0; i < this.state.custHeight; i++) {
+    for (let i = 0; i < this.state.custHeight; i++) {
       let row = [];
-      for(let j = 0; j < this.state.custWidth; j++) {
+      for (let j = 0; j < this.state.custWidth; j++) {
         let style = {};
-        if(this.state.custBlocks.has(i+','+j)) {
-          style = {backgroundColor: 'white'};
-        } else if(this.state.custMirrors.has(i+','+j)) {
-          style = {borderColor: 'white'};
+        if (this.state.custBlocks.has(i + "," + j)) {
+          style = { backgroundColor: "white" };
+        } else if (this.state.custMirrors.has(i + "," + j)) {
+          style = { borderColor: "white" };
         }
-        row.push(<div className='Lobby-square' style={style}> </div>)
+        row.push(
+          <div className="Lobby-square" style={style}>
+            {" "}
+          </div>
+        );
       }
-      grid.push(<div className='Lobby-row'> {row} </div>);
+      grid.push(<div className="Lobby-row"> {row} </div>);
     }
-
 
     if (this.state.initialized) {
       return (
         <>
           <NavBar />
           <div className="Lobby-container">
-            <div className='Lobby-contents'>
+            <div className="Lobby-contents">
               <div className="Lobby-header">
                 <div className="Lobby-heading"> Game Code </div>
                 <div className="Lobby-code"> {code} </div>
               </div>
-              {this.state.creator ? <SettingsBar display={this.state.display} updateDisplay={this.updateDisplay}/> : <hr/> }
-
+              {this.state.creator ? (
+                <SettingsBar display={this.state.display} updateDisplay={this.updateDisplay} />
+              ) : (
+                <hr />
+              )}
 
               {/* LOBBY / GENERAL VIEW SCREEN */}
-              {(this.state.display === 'LOBBY' || !this.state.creator) &&
+              {(this.state.display === "LOBBY" || !this.state.creator) && (
                 <>
                   <div className="Lobby-people">
                     {this.state.players.map((value, index) => (
-                      <div key={index} className="Lobby-username" style={{color: this.getColor(value)}}>
+                      <div
+                        key={index}
+                        className="Lobby-username"
+                        style={{ color: this.getColor(value) }}
+                      >
                         {value}
                       </div>
                     ))}
-
                   </div>
                   {this.state.creator ? (
                     <div>
-                      <div style={{height: 16}}> </div>
+                      <div style={{ height: 16 }}> </div>
                       <div className="u-button" onClick={this.startGame}>
                         {" "}
                         S T A R T{" "}
                       </div>
                     </div>
-
-                  ) : <div className='Lobby-waiting'> waiting for host to start the game. . . </div>}
+                  ) : (
+                    <div className="Lobby-waiting"> waiting for host to start the game. . . </div>
+                  )}
                 </>
-              }
+              )}
 
-              {(this.state.display === 'MAP') && (
+              {this.state.display === "MAP" && (
                 <>
-                  <div className='Lobby-settingContainer'>
-                    <div className='Lobby-settingHeading'> — Choose Your Map Type — </div>
-                    <div className='Lobby-mapTypes'>
-                      <div className='Lobby-mapType' onClick={(e) => {this.setState({standard: true})}} style={this.state.standard ? {backgroundColor: '#363636'} : {backgroundColor: 'black'}}> Standard Map </div>
-                      <div className='Lobby-mapType' onClick={(e) => {this.setState({standard: false})}} style={!this.state.standard ? {backgroundColor: '#363636'} : {backgroundColor: 'black'}}> Custom Map </div>
+                  <div className="Lobby-settingContainer">
+                    <div className="Lobby-settingHeading"> — Choose Your Map Type — </div>
+                    <div className="Lobby-mapTypes">
+                      <div
+                        className="Lobby-mapType"
+                        onClick={(e) => {
+                          this.setState({ standard: true });
+                        }}
+                        style={
+                          this.state.standard
+                            ? { backgroundColor: "#363636" }
+                            : { backgroundColor: "black" }
+                        }
+                      >
+                        {" "}
+                        Standard Map{" "}
+                      </div>
+                      <div
+                        className="Lobby-mapType"
+                        onClick={(e) => {
+                          this.setState({ standard: false });
+                        }}
+                        style={
+                          !this.state.standard
+                            ? { backgroundColor: "#363636" }
+                            : { backgroundColor: "black" }
+                        }
+                      >
+                        {" "}
+                        Custom Map{" "}
+                      </div>
                     </div>
-                    {
-                      this.state.standard ?
-                        (
-                          <div className='Lobby-mapSettings'>
-                            {/* <div className='Lobby-settingTitle'> Map Width: {this.state.stdWidth / 50} </div>
+                    {this.state.standard ? (
+                      <div className="Lobby-mapSettings">
+                        {/* <div className='Lobby-settingTitle'> Map Width: {this.state.stdWidth / 50} </div>
                             <input className='Lobby-slider' type='range' min='1' max='50' value={this.state.stdWidth} onChange={(e) => this.setState({stdWidth: e.target.value})}></input>
                             <div className='Lobby-settingTitle'> Map Height: {this.state.stdHeight / 50} </div>
                             <input className='Lobby-slider' type='range' min='1' max='50' value={this.state.stdHeight} onChange={(e) => this.setState({stdHeight: e.target.value})}></input>
                             <div className='Lobby-settingTitle'> Wall Density: {this.state.stdWallDensity / 50} </div>
                             <input className='Lobby-slider' type='range' min='1' max='50' value={this.state.stdWallDensity} onChange={(e) => this.setState({stdWallDensity: e.target.value})}></input> */}
-                            <div className='Lobby-settingTitle'> Mirror Density: {this.state.stdMirrorDensity / 50} </div>
-                            <input className='Lobby-slider' type='range' min='0' max='50' value={this.state.stdMirrorDensity} onChange={(e) => this.setState({stdMirrorDensity: e.target.value})}></input>
-                          </div>
-                        ) :
-                        <div classname='Lobby-mapSettings'>
-                          <div className='Lobby-settingTitle'> Your Selected Custom Map: {this.state.custTitle === undefined ? 'NOT SELECTED YET' : this.state.custTitle} </div>
-                          <div className='Lobby-gridContainer'>
-                            {grid}
-                          </div>
-                          <div className='u-spacer'> </div>
-                          <div className='Lobby-mapType u-button2' style={{width: '50%', margin: '0 auto'}} onClick={(e) => this.setState({browseMaps: true})}>
-                            { this.state.custTitle === undefined ? 'Browse Maps' : 'Change Map' }
-                          </div>
-
-
+                        <div className="Lobby-settingTitle">
+                          {" "}
+                          Mirror Density: {this.state.stdMirrorDensity / 50}{" "}
                         </div>
-                    }
-
-
+                        <input
+                          className="Lobby-slider"
+                          type="range"
+                          min="0"
+                          max="50"
+                          value={this.state.stdMirrorDensity}
+                          onChange={(e) => this.setState({ stdMirrorDensity: e.target.value })}
+                        ></input>
+                      </div>
+                    ) : (
+                      <div classname="Lobby-mapSettings">
+                        <div className="Lobby-settingTitle">
+                          {" "}
+                          Your Selected Custom Map:{" "}
+                          {this.state.custTitle === undefined
+                            ? "NOT SELECTED YET"
+                            : this.state.custTitle}{" "}
+                        </div>
+                        <div className="Lobby-gridContainer">{grid}</div>
+                        <div className="u-spacer"> </div>
+                        <div
+                          className="Lobby-mapType u-button2"
+                          style={{ width: "50%", margin: "0 auto" }}
+                          onClick={(e) => this.setState({ browseMaps: true })}
+                        >
+                          {this.state.custTitle === undefined ? "Browse Maps" : "Change Map"}
+                        </div>
+                      </div>
+                    )}
                   </div>
-
                 </>
               )}
 
-
-
               {/* GAME SETTINGS SCREEN */}
-              {
-                this.state.display === 'GAME' && (
-                  <div className='Lobby-settingContainer' style={{width: 404, paddingBottom: 32}}>
-                    <div className='Lobby-settingHeading'> — Gameplay Settings — </div>
-                    <div className='Lobby-settingTitle'> Game Duration: {this.state.duration} min </div>
-                    <input className='Lobby-slider' type='range' min='1' max='15' value={this.state.duration} onChange={(e) => {this.setState({duration: e.target.value})}}></input>
-                    <div className='Lobby-settingTitle'> Kill Cooldown: {this.state.cooldown <= 4 ? this.state.cooldown / 4 : this.state.cooldown / 2 -1} sec </div>
-                    <input className='Lobby-slider' type='range' min='1' max='12' value={this.state.cooldown} onChange={(e) => {this.setState({cooldown: e.target.value})}}></input>
-                    <div className='Lobby-settingTitle'> Respawn Time: {this.state.respawn*5} sec </div>
-                    <input className='Lobby-slider' type='range'  min='1' max='12' value={this.state.respawn} onChange={(e) => {this.setState({respawn: e.target.value})}}></input>
-                    <div className='Lobby-settingTitle'> Player Size: {SIZE[this.state.size]} </div>
-                    <input className='Lobby-slider' type='range' min='0' max='2' value={this.state.size} onChange={(e) => {this.setState({size: e.target.value})}}></input>
-                    <div className='Lobby-settingTitle'> Player Speed: {this.state.speed <= 4 ? this.state.speed / 4 : 2*(this.state.speed-4)}x </div>
-                    <input className='Lobby-slider' type='range' min='1' max='6' value={this.state.speed} onChange={(e) => {this.setState({speed: e.target.value})}}></input>
+              {this.state.display === "GAME" && (
+                <div className="Lobby-settingContainer" style={{ width: 404, paddingBottom: 32 }}>
+                  <div className="Lobby-settingHeading"> — Gameplay Settings — </div>
+                  <div className="Lobby-settingTitle">
+                    {" "}
+                    Game Duration: {this.state.duration} min{" "}
                   </div>
-                )
-              }
+                  <input
+                    className="Lobby-slider"
+                    type="range"
+                    min="1"
+                    max="15"
+                    value={this.state.duration}
+                    onChange={(e) => {
+                      this.setState({ duration: e.target.value });
+                    }}
+                  ></input>
+                  <div className="Lobby-settingTitle">
+                    {" "}
+                    Kill Cooldown:{" "}
+                    {this.state.cooldown <= 4
+                      ? this.state.cooldown / 4
+                      : this.state.cooldown / 2 - 1}{" "}
+                    sec{" "}
+                  </div>
+                  <input
+                    className="Lobby-slider"
+                    type="range"
+                    min="1"
+                    max="12"
+                    value={this.state.cooldown}
+                    onChange={(e) => {
+                      this.setState({ cooldown: e.target.value });
+                    }}
+                  ></input>
+                  <div className="Lobby-settingTitle">
+                    {" "}
+                    Respawn Time: {this.state.respawn * 5} sec{" "}
+                  </div>
+                  <input
+                    className="Lobby-slider"
+                    type="range"
+                    min="1"
+                    max="12"
+                    value={this.state.respawn}
+                    onChange={(e) => {
+                      this.setState({ respawn: e.target.value });
+                    }}
+                  ></input>
+                  <div className="Lobby-settingTitle"> Player Size: {SIZE[this.state.size]} </div>
+                  <input
+                    className="Lobby-slider"
+                    type="range"
+                    min="0"
+                    max="2"
+                    value={this.state.size}
+                    onChange={(e) => {
+                      this.setState({ size: e.target.value });
+                    }}
+                  ></input>
+                  <div className="Lobby-settingTitle">
+                    {" "}
+                    Player Speed:{" "}
+                    {this.state.speed <= 4
+                      ? this.state.speed / 4
+                      : 2 * (this.state.speed - 4)}x{" "}
+                  </div>
+                  <input
+                    className="Lobby-slider"
+                    type="range"
+                    min="1"
+                    max="6"
+                    value={this.state.speed}
+                    onChange={(e) => {
+                      this.setState({ speed: e.target.value });
+                    }}
+                  ></input>
+                </div>
+              )}
             </div>
 
-            {this.state.browseMaps ?
-              <MapSelector userId={this.props.userId} selectMap={this.selectMap}/> :
-
+            {this.state.browseMaps ? (
+              <MapSelector userId={this.props.userId} selectMap={this.selectMap} />
+            ) : (
               <ColorPicker
                 code={this.props.code}
                 colorMap={this.state.colors}
                 names={this.state.playerNames}
               ></ColorPicker>
-            }
-
-
+            )}
           </div>
         </>
       );
@@ -330,6 +453,9 @@ class Lobby extends Component {
           <div className="Lobby-container">
             <div className="Lobby-load Lobby-heading u-textCenter"> L O A D I N G . . .</div>
             <div className="Lobby-loading"> </div>
+          </div>
+          <div className="Lobby-container">
+            <div>Try refreshing the page if the lobby doesn't load after a few seconds. </div>
           </div>
         </>
       );
